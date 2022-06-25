8431bebb94f12b513fa9115959ac7b67d2a0acc0
kategrom <95121842+kategrom@users.noreply.github.com>
2022-06-25 12:36:55 +0300

    Add files via upload

---
+++ M3-practic1.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// task 1
+
+// const user = {
+//   userName: 'Katerina',
+//   age: 13,
+//   showUserName() {
+//     console.log(this.userName);
+//   },
+//   updateAge() {
+//     this.age += 1;
+//   }
+// };
+
+// user.showUserName();
+// user.updateAge();
+
+
+// task 2
+
+// const salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+
+// const values = Object.values(salaries);
+
+// let sum = 0;
+
+// for (const value of values) {
+//   sum += value;
+// }
+
+// console.log(sum);
+
+
+// task 3
+
+// const calculator = {
+//   read(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   },
+//   sum() {
+//     return this.a + this.b;
+//   },
+//   mult() {
+//     return this.a * this.b;
+//   }
+// };
+
+// calculator.read(+prompt('Введіть перше число'), +prompt('Введіть друге число'));
+// console.log(calculator.sum());
+// console.log(calculator.mult());
+
+
+// task 4
+
+// const cafe = {
+//   name: "Republic",
+//   width:200,
+//   height: 300,
+//   personal:5,
+//   hr: true 
+// };
+
+// first task
+
+// for(const entry of Object.entries(cafe)){
+//   console.log(entry);
+// };
+
+// last task
+
+// const area = cafe.width * cafe.height;
+// console.log(area);
+
